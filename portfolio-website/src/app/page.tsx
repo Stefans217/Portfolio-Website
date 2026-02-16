d2e4b48fe3f605f0e4fba558fa7701145c8e9b95
Stefan Spataro <stefanspataro@hotmail.com>
2026-02-15 21:50:24 -0800

    just personalize

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <main className="page-container !pt-0">
             {/* Hero Section */}
             <section className="relative min-h-[calc(100vh-120px)] flex items-center justify-center">
-                <Hero name="Stefan Spataro" tagline="A network enthusiast who likes to build software" photoSrc={["/images/ProfessionalVothPhoto.jpg", "/images/MountainPhoto.jpg"]} />
+                <Hero name="Stefan Spataro" tagline="A network enthusiast who likes to build clever software solutions." photoSrc={["/images/ProfessionalVothPhoto.jpg", "/images/MountainPhoto.jpg"]} />
             </section>
         </main>
     );
